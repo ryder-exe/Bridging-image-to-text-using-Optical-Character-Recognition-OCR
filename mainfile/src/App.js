079e13cd0375cda8af7fc971e0e629400942c25f
Ryder <faiz64868@gmail.com>
2023-10-23 21:29:54 +0530

    commit 3

---
+++ mainfile/src/App.js
@@ -0,0 +1,82 @@
+import { useCallback, useEffect, useState } from 'react';
+import { createWorker } from 'tesseract.js';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
+function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [textResult, setTextResult] = useState("");
+
+  const worker = createWorker();
+
+  const convertImageToText = useCallback(async () => {
+    if(!selectedImage) return;
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const { data } = await worker.recognize(selectedImage);
+    setTextResult(data.text);
+  }, [worker, selectedImage]);
+
+  useEffect(() => {
+    convertImageToText();
+  }, [selectedImage, convertImageToText])
+
+  const handleChangeImage = e => {
+    if(e.target.files[0]) {
+      setSelectedImage(e.target.files[0]);
+      toast.info('Processing the Image...', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+      
+    } else {
+      setSelectedImage(null);
+      setTextResult("")
+    }
+  }
+
+  return (
+    <div className="App">
+       <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        />
+      <div className='header'>Optical Character Recognition :<span>Image to Text Convertor</span></div>
+      <div className="input-wrapper">
+        <label for="upload">Upload Image</label>
+        <input type="file" id="upload" accept='image/*' onChange={handleChangeImage} />
+      </div>
+
+      <div className="result">
+        {selectedImage && (
+          <div className="box-image">
+            <img src={URL.createObjectURL(selectedImage)} alt="thumb" />
+          </div>
+        )}
+        {textResult && (
+          <div className="box-p">
+            <p>{textResult}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
